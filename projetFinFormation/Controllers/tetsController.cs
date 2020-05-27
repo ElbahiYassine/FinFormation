8bebcab8758e630cd4b600f065ad902be7abef41
yassinelebahi <yassinelebahi@gmail.com>
2020-05-27 22:14:19 +0000

    10

---
+++ projetFinFormation/Controllers/tetsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projetFinFormation.Controllers
+{
+    public class tetsController : Controller
+    {
+        // GET: tets
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
